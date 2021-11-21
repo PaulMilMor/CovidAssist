51834902d55b460c05fb4d5978c9526917a83fd7
Paul Millanes <pauljmm99@gmail.com>
2021-11-20 17:54:08 -0700

    Probando una BottomNavigationBar

--- app/src/main/java/com/josemillanes/covidassist/MainActivity.java
+++ app/src/main/java/com/josemillanes/covidassist/MainActivity.java
@@ -16,35 +16,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView usernameText;
-    private TextView userEmailText;
-    private ImageView userImage;
-    private FirebaseConnection firebaseConnection = new FirebaseConnection();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        usernameText = (TextView) findViewById(R.id.usernameText);
-        userEmailText = (TextView) findViewById(R.id.userEmailText);
-        userImage   = (ImageView) findViewById(R.id.userImage);
-
-        firebaseConnection.startFirestore();
-        List<Usuario> userList = firebaseConnection.getUsers(usernameText, userEmailText,userImage);
-
-       // usernameText.setText(""+userList.size());
-     /*   usernameText.setText(userList.get(0).getUserName());
-        userEmailText.setText(userList.get(0).getUserEmail());
-        try {
-            URL url = new URL(userList.get(0).getUserImg());
-            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-            userImage.setImageBitmap(bmp);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
 
     }
 }
